46b51309f9e4eb577e69958b8593ee54fda5aee2
M Mansour <64868740+moa-mnsr@users.noreply.github.com>
2025-02-20 20:13:25 +0100

    javascipt updates

--- script.js
+++ script.js
@@ -1,108 +1,31 @@
-// FIRST COLUMN OF POEM - in the code, we will call this box "Hippo"
-
-// data for holding which line we are on
-let hippoNumber = 0
-
-// holding the content for each line
-const hippos = [
-  { text: "not a portrait bare"},
-  { text: "჻"},
-  { text: "public waters"},
-  { text: "჻჻"},
-  { text: "of dying perfect"},
-  { text: "჻"},
-  { text: "how is care"},
-  { text: "at our sleep"},
-  { text: "჻"},
-  { text: "჻჻"},
-  { text: "it was good to have"},
-  { text: "a few"},
-  { text: "჻"},
-  { text: "sometimes learnt"},
-  { text: "a thing"},
-  { text: "჻"},
-  { text: "the room to realize"},
-  { text: "჻"},
-  { text: "჻჻"},
-  { text: "each as it yearns"},
-]
-
-// picking the relevant tags
-const hippoTag = document.querySelector("section.hippo")
-
-// making a next function to increase LineANumber
-const nextHippo = function () {
-  hippoNumber = hippoNumber + 1
-
-  if (hippoNumber > hippos.length - 1) {
-    hippoNumber = 0
-  }
-
-  updateHippo()
-}
-
-// updating the content of the section
-const updateHippo = function () {
-  hippoTag.innerHTML = hippos[hippoNumber].text
-}
-
-// on click of box, run this
-hippoTag.addEventListener("click", function () {
-  nextHippo()
-})
-
-// SECOND COLUMN OF POEM - in the code, we will call this box "Zebra"
-
-// data fr holding which line we are on
-let zebraNumber = 0
-
-// holding the content for each line
-const zebras = [
-  { text: "but knew"},
-  { text: "own tooth"},
-  { text: "჻"},
-  { text: "჻჻"},
-  { text: "჻჻჻"},
-  { text: "We write about"},
-  { text: "the blue not needed"},
-  { text: "჻"},
-  { text: "And how sad to think"},
-  { text: "჻"},
-  { text: "჻჻"},
-  { text: "years’ glances"},
-  { text: "჻"},
-  { text: "჻჻"},
-  { text: "჻჻჻"},
-  { text: "჻჻჻჻"},
-  { text: "჻჻჻჻჻"},
-  { text: "of a neckline and distillery"},
-  { text: "჻"},
-  { text: "჻჻"},
-  { text: "but knew"},
-
+document.addEventListener("DOMContentLoaded", function() {
   
-]
+  const gridItems = document.querySelectorAll(".grid-item");
 
-// picking the relevant tags
-const zebraTag = document.querySelector("section.zebra")
+  const stanzaLists = [
+    ["pick — mistake"],
+    ["of us a promise"],
+    ["or so, they say"],
+    ["failure is"]
+  ];
 
-// making a next function to increase LineANumber
-const nextZebra = function () {
-  zebraNumber = zebraNumber + 1
+  gridItems.forEach((item, index) => {
+    item.dataset.index = 0;
+    item.dataset.originalText = item.textContent;
 
-  if (zebraNumber > zebras.length - 1) {
-    zebraNumber = 0
-  }
+    item.addEventListener("click", function () {
+      let currentIndex = Number(this.dataset.index);
 
-  updateZebra()
-}
+      if (currentIndex === 0) {
+        this.textContent = stanzaLists[index][0];
+        this.dataset.index = 1;
+      } else {
+        this.textContent = this.dataset.originalText;
+        this.dataset.index = 0;
+      }
+      
+    });
 
-// updating the content of the section
-const updateZebra = function () {
-  zebraTag.innerHTML = zebras[zebraNumber].text
-}
+  });
 
-// on click of box, run this
-zebraTag.addEventListener("click", function () {
-  nextZebra()
-})
+});
